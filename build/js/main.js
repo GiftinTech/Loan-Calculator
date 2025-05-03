4dca609f680453bad58bbd1ba93d70c2e35f9a6e
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-05-03 08:13:53 +0100

    Did the logic for the project

---
+++ build/js/main.js
@@ -0,0 +1,31 @@
+"use strict";
+const calculateLoan = ({ amount, interestRate, years }) => {
+    const principal = amount;
+    const calculatedInterest = interestRate / 100 / 12;
+    const calculatedPayment = years * 12;
+    const monthlyInterest = calculatedInterest * principal;
+    const interestGrowth = (1 + calculatedInterest) ** calculatedPayment;
+    const monthly = (principal * interestGrowth * calculatedInterest) / (interestGrowth - 1);
+    const total = monthly * calculatedPayment;
+    const interest = total - principal;
+    return {
+        monthlyPayment: Number(monthly.toFixed(2)),
+        monthlyInterest: Number(monthlyInterest.toFixed()),
+        totalInterest: Number(interest.toFixed(2)),
+        totalPayment: Number(total.toFixed(2))
+    };
+};
+const renderPage = (value) => {
+    let userInput = value;
+    if (userInput >= 500 || !userInput === null) {
+        console.log(calculateLoan({
+            amount: value,
+            interestRate: 12,
+            years: 1
+        }));
+    }
+    else {
+        console.log('Invalid Input');
+    }
+};
+renderPage(500);
