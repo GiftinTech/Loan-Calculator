847b7a46ecc4a879fd2b042628eb290e6ffc66a1
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-05-15 13:38:23 +0100

    Move css to src/ + change build/ to public/ for Vite server

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',               // use current directory as root
-  base: './',              // ensures relative paths
+  root: '.', // use current directory as root
+  base: './', // ensures relative paths
   build: {
-    outDir: 'build',        // output directory
-    emptyOutDir: true      // clean before build
-  }
-})
+    outDir: 'build', // output directory
+    emptyOutDir: true, // clean before build
+  },
+});
