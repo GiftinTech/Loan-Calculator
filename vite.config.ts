74d26bdbaae3bde752e9001f70c3c09ee17383eb
Gift Egbonyi <egbonyigiftvicky@gmail.com>
2025-05-18 23:39:47 +0100

    Set up GitHub Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: '.', // use current directory as root
-  base: './', // ensures relative paths
+  base: '/Loan-Calculator/', // ensures relative paths
   build: {
-    outDir: 'build', // output directory
+    outDir: 'dist', // output directory
     emptyOutDir: true, // clean before build
   },
   test: {
